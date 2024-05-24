03548581d760b002cc374be19176a6dd09bd0ac9
nichhal <154792318+nichhal@users.noreply.github.com>
2024-05-24 17:32:31 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,5 @@
+MODEL_FILE_PATH = r'C:\Users\Destiny\New folder\12_23_Flask_medical_insurance\project_app\Linear_Model.pkl'
+
+JSON_FILE_PATH = r'C:\Users\Destiny\New folder\12_23_Flask_medical_insurance\project_app\project_data.json'
+
+PORT_NUMBER = 5004
